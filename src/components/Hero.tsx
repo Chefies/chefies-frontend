47fa872468854273fb3a1a21a2f50e1533710c21
Emyr298 <emi.ru298@gmail.com>
2024-06-20 14:50:04 +0700

    feat: add download link and change image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
-import { IoMdDownload } from "react-icons/io";
-import phoneImage from "../assets/phone.png";
+import { IoLogoAndroid } from "react-icons/io";
+import phoneImage from "../assets/what-ingredients.png";
 
 function Hero() {
   return (
     <div className="flex md:flex-row-reverse flex-col justify-center gap-16 mx-auto">
       <div className="p-4 drop-shadow-xl mx-auto">
-        <img src={phoneImage} className="h-[60vh]" />
+        <img src={phoneImage} alt="Hai Chef, what ingredients do you have today?" className="h-[60vh]" />
       </div>
 
       <div className="flex flex-col md:justify-center md:text-end uppercase gap-4 max-w-lg">
@@ -15,9 +15,14 @@ function Hero() {
           healthy recipes with the ingredients you have, we got it covered!
         </p>
         <p>Powered by AI.</p>
-        <button className="bg-primary text-white px-16 py-4 w-fit rounded-2xl ml-auto uppercase flex gap-2 items-center justify-center">
-          <IoMdDownload className="w-6 h-6" />
-          <p className="pt-1 font-bold">Download</p>
+        <button
+          className="bg-primary text-white px-16 py-4 w-fit rounded-2xl ml-auto uppercase flex gap-2 items-center justify-center"
+          onClick={() => {
+            window.open("https://sendgb.com/jEgcfze9BtC", "_blank")
+          }}
+        >
+          <IoLogoAndroid className="w-6 h-6" />
+          <p className="pt-1 font-bold">Download APK</p>
         </button>
       </div>
     </div>
